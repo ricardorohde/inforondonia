b35b715dcf8b19ac318180ff8d079125f373c852
gean.bertani <gean.bertani@suporte12.atimonet.com.br>
2016-04-02 10:53:23 -0400

    Update - v.1.2  -- Layout form Denúncia

--- themes/inforondonia/denuncia.php
+++ themes/inforondonia/denuncia.php
@@ -7,7 +7,22 @@
                 <span class="header_line"></span>
             </header>
             <article class="content_pag">
-
+                <div class="box_denuncia">
+                    <div class="denuncia_txt">
+                        Faça agora mesmo sua denúncia. Mandem-nos fotos, vídeos, reclamações, sugestões, sobre o que esta acontecendo com seu município, todas as denúncias serão investigadas e se forem verídicas serão feitas matérias para mostrar o descaso, e as sugestões serão sempre bem vidas para a melhoria do <b>PORTAL <?= SITENAME;?></b>.<br> Seus dados serão guardados em sigílo e não aparecerão para o público. 
+                    </div>
+                    <div class="denuncia_form">
+                        <form>
+                            <input type="text" name="denuncia_nome" placeholder="Informe seu Nome" class="denuncia_nome">
+                            <input type="text" name="denuncia_email" placeholder="Informe seu E-mail" class="denuncia_email">
+                            <input type="text" name="denuncia_cidade" placeholder="Informe sua Cidade" class="denuncia_cidade">
+                            <input type="text" name="denuncia_uf" placeholder="UF" class="denuncia_uf">
+                            <input type="text" name="denuncia_telefone" placeholder="Informe seu Telefone" class="denuncia_telefone">
+                            <textarea rows="10" name="denuncia_msg" placeholder="Deixe sua Mensagem" class="denuncia_msg"></textarea>
+                            <input type="text" name="denuncia_link" placeholder="Link do Video" class="denuncia_link">
+                        </form>
+                    </div>
+                </div>
             </article>
         </div>
     </div>
