c926e392954ce9a4aaae3bf0dae235186b35871b
gean.bertani <gean.bertani@suporte12.atimonet.com.br>
2016-04-07 11:08:55 -0400

    Update - v.1.11  - Edição da Classe Cotacao.class.php

--- _app/Helpers/Cotacao.class.php
+++ _app/Helpers/Cotacao.class.php
@@ -4,39 +4,40 @@
  * Contacao.class [ HELPER ]
  * Classe responável por capturar a contação do dolar e euro!
  * 
- * @copyright (c) 2014, Gean M S Bertani CREATIVE WEBSITES
+ * @copyright (c) 2016, Gean M S Bertani CREATIVE WEBSITES
  */
 class Cotacao {
 
-    private static $apiUrl = "http://developers.agenciaideias.com.br/cotacoes/json";
-    private static $content;
+    private $Data;
+    private $Error;
+    private $Result;
 
-    //Iniciar GET dos dados
-    public static function init() {
-        self::$content = file_get_contents(self::$apiUrl);
-        self::$content = json_decode(self::$content);
+    //Url da API
+    const ApiUrl = 'http://developers.agenciaideias.com.br/cotacoes/json';
+
+    /**
+     * <b>Inciar Dados:</b> Iniciar GET dos dados!
+     */
+    function __construct() {
+        if (!file_get_contents(self::ApiUrl)):
+            $this->Error = ["Não foi possivel estabelecer conexão com a API.", WS_ALERT];
+            $this->Result = false;
+        else:
+            $this->Data = file_get_contents(self::ApiUrl);
+            $this->Data = json_decode($this->Data, TRUE);
+            $this->Result = true;
+        endif;
     }
 
     //Return Array Dolar
-    public static function Dolar() {
-        self::init();
-        $cotacao = str_replace('.', ',', self::$content->dolar->cotacao);
+    public function Dolar() {
+        $cotacao = str_replace('.', ',', $this->Data->dolar->cotacao);
         return array(
+            "tipo" => 'dolar',
             "cotacao" => $cotacao,
             "variacao" => self::$content->dolar->variacao,
             "status" => self::getVariation(self::$content->dolar->variacao),
-            "atualizado"=>self::$content->atualizacao
-        );
-    }
-
-    //Return Array Bovespa
-    public static function Bovespa() {
-        self::init();
-        $cotacao = str_replace('.', ',', self::$content->bovespa->cotacao);
-        return array(
-            "cotacao" => $cotacao,
-            "variacao" => self::$content->bovespa->variacao,
-            "status" => self::getVariation(self::$content->bovespa->variacao)
+            "atualizado" => self::$content->atualizacao
         );
     }
 
@@ -45,11 +46,49 @@ class Cotacao {
         self::init();
         $cotacao = str_replace('.', ',', self::$content->euro->cotacao);
         return array(
+            "tipo" => 'euro',
             "cotacao" => $cotacao,
             "variacao" => self::$content->euro->variacao,
-            "status" => self::getVariation(self::$content->euro->variacao)
+            "status" => self::getVariation(self::$content->euro->variacao),
+            "atualizado" => self::$content->atualizacao
         );
     }
+
+    //Return Array Bovespa
+    public static function Bovespa() {
+        self::init();
+        $cotacao = str_replace('.', ',', self::$content->bovespa->cotacao);
+        return array(
+            "tipo" => 'bovespa',
+            "cotacao" => $cotacao,
+            "variacao" => self::$content->bovespa->variacao,
+            "status" => self::getVariation(self::$content->bovespa->variacao),
+            "atualizado" => self::$content->atualizacao
+        );
+    }
+
+    /**
+     * <b>Verificar Dados:</b> Retorna TRUE se o cadastro ou update for efetuado ou FALSE se não.
+     * Para verificar erros execute um getError();
+     * @return BOOL $Var = True or False
+     */
+    function getResult() {
+        return $this->Result;
+    }
+
+    /**
+     * <b>Obter Erro:</b> Retorna um array associativo com um erro e um tipo.
+     * @return ARRAY $Error = Array associatico com o erro
+     */
+    function getError() {
+        return $this->Error;
+    }
+
+    /*
+     * ***************************************
+     * **********  PRIVATE METHODS  **********
+     * ***************************************
+     */
 
     //Return Return getVariation
     public static function getVariation($variation) {
@@ -59,4 +98,5 @@ class Cotacao {
             return "down";
         endif;
     }
+
 }
---
+++ themes/inforondonia/cotacao.php
@@ -0,0 +1,5 @@
+<?php
+$cotacao = new Cotacao;
+echo "<pre>";
+var_dump($cotacao);
+echo "</pre>";
