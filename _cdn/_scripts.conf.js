5cf7554ce836cf0eda22623d7dfcb14d89314fde
Gean <Gean@SERVIDOR>
2016-04-27 19:19:53 -0400

    Update - v.1.53  -- Google Analystics e redirecionamentos

--- _cdn/_scripts.conf.js
+++ _cdn/_scripts.conf.js
@@ -22,3 +22,19 @@ $(function () {
     js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+//::: Google Analystics :::
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-60249887-3', 'auto');
+ga('send', 'pageview');
