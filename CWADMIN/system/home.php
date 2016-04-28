18116aa460ba6a030faaf5d53d5f423053876ef5
gean.bertani <gean.bertani@suporte12.atimonet.com.br>
2016-04-28 17:11:44 -0400

    Update - v.1.56  - fix CWADMIN add widget INFO Statics.

--- CWADMIN/system/home.php
+++ CWADMIN/system/home.php
@@ -12,6 +12,75 @@
 
 <!-- Main content -->
 <section class="content">
-
-
+    <?php
+    $Today = date('Y-m-d');    
+    $ReadDash = new Read;
+    ?>
+    
+    <div class="row">
+        <div class="col-md-3">
+            <div class="info-box">
+                <?php
+                $Views = $ReadDash;
+                $Views->ExeRead('ws_siteviews', "WHERE siteviews_date = :today", "today={$Today}");
+                $CountViews = $Views->getResult()[0];
+                ?>
+                <div class="info-box-icon bg-yellow">
+                    <i class="fa fa-line-chart"></i>
+                </div>
+                <div class="info-box-content">
+                    <span class="info-box-text">Visitas Hoje</span>
+                    <span class="info-box-number"><?= $CountViews['siteviews_users'];?></span>                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="info-box">
+                <?php
+                $News = $ReadDash;
+                $News->FullRead("SELECT count(*) AS count FROM noticias");
+                $CountNews = $News->getResult()[0];
+                ?>
+                <div class="info-box-icon bg-green">
+                    <i class="fa fa-newspaper-o"></i>
+                </div>
+                <div class="info-box-content">
+                    <span class="info-box-text">Noticias Cadastradas</span>
+                    <span class="info-box-number"><?= $CountNews['count'];?></span>                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="info-box">
+                <?php
+                $Events = $ReadDash;
+                $Events->FullRead("SELECT count(*) AS count FROM eventos");
+                $CountEvents = $Events->getResult()[0];
+                ?>
+                <div class="info-box-icon bg-aqua-active">
+                    <i class="fa fa-camera-retro"></i>
+                </div>
+                <div class="info-box-content">
+                    <span class="info-box-text">Eventos Cadastrados</span>
+                    <span class="info-box-number"><?= $CountEvents['count'];?></span>                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="info-box">
+                <?php
+                $Videos = $ReadDash;
+                $Videos->FullRead("SELECT count(*) AS count FROM videos");
+                $CountVideos = $Videos->getResult()[0];
+                ?>
+                <div class="info-box-icon bg-red">
+                    <i class="fa fa-play-circle"></i>
+                </div>
+                <div class="info-box-content">
+                    <span class="info-box-text">Vídeos Cadastrados</span>
+                    <span class="info-box-number"><?= $CountVideos['count'];?></span>                    
+                </div>
+            </div>
+        </div>
+    </div>
 </section><!-- /.content -->
