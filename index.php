8878b3b8fc2e37238d8af3715274e9a4024ab779
gean.bertani <gean.bertani@suporte12.atimonet.com.br>
2016-05-10 10:33:43 -0400

    Update - v.1.90  - Adicionado fonts locais do google e font_awesome

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ Check::UserOnline();
         $Link->getTags();
         ?>
         <!-- CSS -->
-        <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Oxygen:400,700'>
-        <link rel="stylesheet" type='text/css' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel='stylesheet' type='text/css' href='<?= INCLUDE_PATH; ?>/css/font_oxygen.css'>
+        <link rel="stylesheet" type='text/css' href="<?= HOME; ?>/_cdn/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="<?= INCLUDE_PATH; ?>/css/style.min.css"/>
     </head>
     <body>
