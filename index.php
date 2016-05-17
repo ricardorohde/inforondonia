6c75750298ff080ff53db6d7a4ba2c1764eb17c7
gean.bertani <gean.bertani@suporte12.atimonet.com.br>
2016-05-17 11:06:36 -0400

    Commit - v.1.103  -Trocado o include do GA

--- index.php
+++ index.php
@@ -20,6 +20,22 @@ Check::UserOnline();
         <link rel='stylesheet' type='text/css' href='<?= INCLUDE_PATH; ?>/css/font_oxygen.css'>
         <link rel="stylesheet" type='text/css' href="<?= HOME; ?>/_cdn/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="<?= INCLUDE_PATH; ?>/css/style.min.css"/>
+        <script>
+            //::: Google Analytics:::
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments);
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m);
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+            ga('create', 'UA-60249887-3', 'auto');
+            ga('send', 'pageview');
+        </script>
     </head>
     <body>
         <?php
