00dc2a8f80232e42f85f63b16186ed2ab0834baf
Gean <Gean@SERVIDOR>
2016-05-23 23:26:16 -0400

    Commit - v.1.107  - Ajustes

--- index.php
+++ index.php
@@ -20,24 +20,22 @@ Check::UserOnline();
         <link rel='stylesheet' type='text/css' href='<?= INCLUDE_PATH; ?>/css/font_oxygen.css'>
         <link rel="stylesheet" type='text/css' href="<?= HOME; ?>/_cdn/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="<?= INCLUDE_PATH; ?>/css/style.min.css"/>
-        <script>
-            //::: Google Analytics:::
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments);
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                        m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m);
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-            ga('create', 'UA-60249887-3', 'auto');
-            ga('send', 'pageview');
-        </script>
     </head>
     <body>
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NVRPHT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>
+            (function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-NVRPHT');
+        </script>
+        <!-- End Google Tag Manager -->
+
         <?php
         //Topo
         require(REQUIRE_PATH . '/inc/header.inc.php');
