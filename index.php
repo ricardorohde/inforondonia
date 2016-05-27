b11b39c0652912b2be1a88e2a04c67cca566380e
Gean <Gean@SERVIDOR>
2016-05-27 19:51:44 -0400

    Commit - v.1.109  - Configuação de Outra Conta do GA

--- index.php
+++ index.php
@@ -48,6 +48,23 @@ Check::UserOnline();
         //Rodape
         require(REQUIRE_PATH . '/inc/footer.inc.php');
         ?>
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-78466412-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </body>
     <!-- JS -->
     <script src="<?= HOME; ?>/_cdn/jquery.js"></script>
