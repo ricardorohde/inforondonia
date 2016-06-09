4acd43f0a44bfa8bad92e5c8e56b4ec5935d6f1a
Gean <Gean@SERVIDOR>
2016-06-09 00:48:44 -0400

    Commit - v.1.118  - Ajustes

--- index.php
+++ index.php
@@ -20,19 +20,6 @@ Check::UserOnline();
         <link rel="stylesheet" type="text/css" href="<?= INCLUDE_PATH; ?>/css/style.min.css"/>
     </head>
     <body>
-        <!-- Google Tag Manager -->
-        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NVRPHT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <script>
-            (function (w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-                var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                j.async = true;
-                j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-NVRPHT');
-        </script>
-        <!-- End Google Tag Manager -->
         <script>
             (function (i, s, o, g, r, a, m) {
                 i['GoogleAnalyticsObject'] = r;
@@ -50,6 +37,19 @@ Check::UserOnline();
             ga('linker:autoLink', ['inforondonia.com', 'portalinforondonia.com.br', 'informarondonia.com'])
             ga('send', 'pageview');
         </script>
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NVRPHT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>
+            (function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-NVRPHT');
+        </script>
+        <!-- End Google Tag Manager -->
         <?php
         //Topo
         require(REQUIRE_PATH . '/inc/header.inc.php');
