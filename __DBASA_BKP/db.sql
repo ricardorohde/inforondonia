726e625e37afb5ad8db07cfa9c374cedce64aa5a
gean.bertani <gean.bertani@suporte12.atimonet.com.br>
2016-09-21 14:35:53 -0400

    v.1.0.144 - 21/09/2016 --Refresh banco de dados

--- __DBASA_BKP/db.sql
+++ __DBASA_BKP/db.sql
@@ -2,7 +2,7 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           5.6.21 - MySQL Community Server (GPL)
 -- OS do Servidor:               Win32
--- HeidiSQL Versão:              9.3.0.5107
+-- HeidiSQL Versão:              9.3.0.5117
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -1280,6 +1280,82 @@ REPLACE INTO `banners_tipo` (`id_tipo`, `tipo`, `dimens_w`, `dimens_h`) VALUES
 	(13, 'Pub. Noticia Ler 1', '870', '140'),
 	(14, 'Pub. Noticia Ler 2', '870', '140');
 /*!40000 ALTER TABLE `banners_tipo` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela _inforondonia_2016.clima_dados
+DROP TABLE IF EXISTS `clima_dados`;
+CREATE TABLE IF NOT EXISTS `clima_dados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cidade` varchar(100) NOT NULL,
+  `uf` char(2) NOT NULL,
+  `data_atualizado` date NOT NULL,
+  `data_previsao` date NOT NULL,
+  `id_sigla` int(11) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `temp_max` int(2) NOT NULL,
+  `temp_min` int(2) NOT NULL,
+  `iuv` int(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_clima_dados_id_sigla` (`id_sigla`),
+  CONSTRAINT `fk_clima_dados_id_sigla` FOREIGN KEY (`id_sigla`) REFERENCES `clima_sigla` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Dados sobre o clima';
+
+-- Copiando dados para a tabela _inforondonia_2016.clima_dados: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `clima_dados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clima_dados` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela _inforondonia_2016.clima_sigla
+DROP TABLE IF EXISTS `clima_sigla`;
+CREATE TABLE IF NOT EXISTS `clima_sigla` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sigla` varchar(3) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COMMENT='Sigla das condições do clima';
+
+-- Copiando dados para a tabela _inforondonia_2016.clima_sigla: ~40 rows (aproximadamente)
+/*!40000 ALTER TABLE `clima_sigla` DISABLE KEYS */;
+REPLACE INTO `clima_sigla` (`id`, `sigla`, `descricao`) VALUES
+	(1, 'ec', 'Encoberto com Chuvas Isoladas'),
+	(2, 'ci', 'Chuvas Isoladas'),
+	(3, 'c', 'Chuva'),
+	(4, 'in', 'Instável'),
+	(5, 'pp', 'Poss. de Pancadas de Chuva'),
+	(6, 'cm', 'Chuva pela Manhã'),
+	(7, 'cn', 'Chuva a Noite'),
+	(8, 'pt', 'Pancadas de Chuva a Tarde'),
+	(9, 'pm', 'Pancadas de Chuva pela Manhã'),
+	(10, 'np', 'Nublado e Pancadas de Chuva'),
+	(11, 'pc', 'Pancadas de Chuva'),
+	(12, 'pn', 'Parcialmente Nublado'),
+	(13, 'cv', 'Chuvisco'),
+	(14, 'ch', 'Chuvoso'),
+	(15, 't', 'Tempestade'),
+	(16, 'ps', 'Predomínio de Sol'),
+	(17, 'e', 'Encoberto'),
+	(18, 'n', 'Nublado'),
+	(19, 'cl', 'Céu Claro'),
+	(20, 'nv', 'Nevoeiro'),
+	(21, 'g', 'Geada'),
+	(22, 'ne', 'Neve'),
+	(23, 'nd', 'Não Definido'),
+	(24, 'pnt', 'Pancadas de Chuva a Noite'),
+	(25, 'psc', 'Possibilidade de Chuva'),
+	(26, 'pcm', 'Possibilidade de Chuva pela Manhã'),
+	(27, 'pct', 'Possibilidade de Chuva a Tarde'),
+	(28, 'pcn', 'Possibilidade de Chuva a Noite'),
+	(29, 'npt', 'Nublado com Pancadas a Tarde'),
+	(30, 'npn', 'Nublado com Pancadas a Noite'),
+	(31, 'ncn', 'Nublado com Poss. de Chuva a Noite'),
+	(32, 'nct', 'Nublado com Poss. de Chuva a Tarde'),
+	(33, 'ncm', 'Nubl. c/ Poss. de Chuva pela Manhã'),
+	(34, 'npm', 'Nublado com Pancadas pela Manhã'),
+	(35, 'npp', 'Nublado com Possibilidade de Chuva'),
+	(36, 'vn', 'Variação de Nebulosidade'),
+	(37, 'ct', 'Chuva a Tarde'),
+	(38, 'ppn', 'Poss. de Panc. de Chuva a Noite'),
+	(39, 'ppt', 'Poss. de Panc. de Chuva a Tarde'),
+	(40, 'ppm', 'Poss. de Panc. de Chuva pela Manhã');
+/*!40000 ALTER TABLE `clima_sigla` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela _inforondonia_2016.colunistas
 DROP TABLE IF EXISTS `colunistas`;
