ef5ac2fb55a34b1eb92653517609e75a445884b2
Gean <Gean@SERVIDOR>
2016-09-28 22:42:21 -0400

    v.1.0.152 - 28/09/2016  - Criado busca integrada a paginação, módulo de noticias.

--- CWADMIN/system/noticias/listar.php
+++ CWADMIN/system/noticias/listar.php
@@ -50,10 +50,26 @@ $tipo = ['N', 'Notícias'];
                             <a href="painel.php?exe=<?= $case[0]; ?>/cadastrar" class="btn btn-success btn-new"><span class="fa fa-file"></span>  </a>
                         </div>
                         <div class="col-xs-10 col-md-6 col-md-offset-4">
-                            <form name="FrmBusca" method="get" action="">
+                            <?php
+                            $search = filter_input(INPUT_POST, 's', FILTER_DEFAULT);
+                            $searchSubmit = filter_input(INPUT_POST, 'searchSubmit', FILTER_DEFAULT);
+                            if (!empty($search)):
+                                $search = strip_tags(trim(urlencode($search)));
+                                header("Location: painel.php?exe={$exe}&search={$search}");
+                            else:
+                                if (!empty($searchSubmit)):
+                                    //Redireciona quando não for pesquisado nada.
+                                    header("Location: painel.php?exe={$exe}");
+                                endif;
+                            endif;
+                            $getSearch = filter_input(INPUT_GET, 'search', FILTER_DEFAULT);
+                            ?>
+                            <form name="FrmBusca" method="POST">
                                 <div class="input-group">
-                                    <input type="text" name="search" id="busca" class="form-control" placeholder="Faça a busca dos registros">
-                                    <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                                    <input type="text" name="s" id="busca" class="form-control" placeholder="Faça a busca dos registros">
+                                    <span class="input-group-btn">
+                                        <button type="submit" name="searchSubmit" value="Buscar" class="btn btn-primary"><i class="fa fa-search"></i></button>
+                                    </span>
                                 </div>
                             </form>
                         </div>
@@ -63,17 +79,31 @@ $tipo = ['N', 'Notícias'];
                 <div class="box-body">
                     <div class="row">
                         <?php
+                        $linkPage = !empty($getSearch) ? "painel.php?exe={$exe}&search={$getSearch}&page=" : "painel.php?exe={$exe}&page=";
                         $getPage = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-                        $Pager = new Pager("painel.php?exe={$exe}&page=");
+                        $Pager = new Pager($linkPage);
                         $Pager->ExePager($getPage, 12);
 
+                        if (!empty($getSearch)):
+                            //Listagem de Rows
+                            $WhereRows = " WHERE n.titulo != :t AND (titulo LIKE '%' :link '%') ORDER BY n.id DESC LIMIT :limit OFFSET :offset";
+                            $TermosRows = "t=''&link={$getSearch}&limit={$Pager->getLimit()}&offset={$Pager->getOffset()}";
+                            //Paginação
+                            $WherePag = " WHERE n.titulo != :t AND (titulo LIKE '%' :link '%') ORDER BY n.id DESC";
+                            $TermosPag = "t=''&link={$getSearch}";
+                        else:
+                            //Listagem de Rows
+                            $WhereRows = " WHERE n.titulo != :t ORDER BY n.id DESC LIMIT :limit OFFSET :offset";
+                            $TermosRows = "t=''&limit={$Pager->getLimit()}&offset={$Pager->getOffset()}";
+                            //Paginação
+                            $WherePag = " WHERE n.titulo != :t ORDER BY n.id DESC";
+                            $TermosPag = "t=''";
+                        endif;
                         $readRows->FullRead(
                                 "  SELECT n.id, n.url_name, n.foto, n.titulo, n.data, n.coluna, n.destaque, n.contador, u.nome, u.sexo"
                                 . "  FROM noticias n "
                                 . "  LEFT JOIN usuarios u ON n.qm_cadastr=u.id"
-                                . " WHERE n.titulo != :t"
-                                . " ORDER BY id DESC "
-                                . " LIMIT :limit OFFSET :offset", "t=''&limit={$Pager->getLimit()}&offset={$Pager->getOffset()}");
+                                . " {$WhereRows}", "{$TermosRows}");
                         if (!$readRows->getResult()):
                             WSErro('Ainda não há nenhum registro...', WS_INFOR);
                         else:
@@ -132,7 +162,7 @@ $tipo = ['N', 'Notícias'];
                         </div>
                         <div class="col-md-7">
                             <?php
-                            $Pager->ExePaginator("noticias");
+                            $Pager->ExePaginator("noticias n", $WherePag, $TermosPag);
                             echo $Pager->getPaginator();
                             ?>
                         </div>
