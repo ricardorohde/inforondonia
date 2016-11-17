925ae39fc94294e1f6bcb9b4738f284e2e9b1f90
Gean <Gean@SERVIDOR>
2016-11-17 02:08:34 -0400

    v.1.0.165 - 17/11/2016  - Configuração do CKFinder para enviar fotos no editor CKEditor. Foi solicitado ao suporte para ativar a extension php_fileinfo.dll

--- CWADMIN/plugins/ckeditor/config.js
+++ CWADMIN/plugins/ckeditor/config.js
@@ -6,7 +6,12 @@
 CKEDITOR.editorConfig = function (config) {
     config.language = 'pt-br';
     config.skin = 'icy_orange';
-    config.filebrowserImageBrowseLinkUrl = '/browser/browse.php';
+    config.filebrowserBrowseUrl = 'plugins/ckeditor/ckfinder/ckfinder.html',
+            config.filebrowserImageBrowseUrl = 'plugins/ckeditor/ckfinder/ckfinder.html?type=Images',
+            config.filebrowserFlashBrowseUrl = 'plugins/ckeditor/ckfinder/ckfinder.html?type=Flash',
+            config.filebrowserUploadUrl = 'plugins/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
+            config.filebrowserImageUploadUrl = 'plugins/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+            config.filebrowserFlashUploadUrl = 'plugins/ckeditor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 
     config.toolbarGroups = [
         {name: 'document', groups: ['mode', 'document', 'doctools']},
@@ -14,12 +19,12 @@ CKEDITOR.editorConfig = function (config) {
         {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
         {name: 'tools', groups: ['tools']},
         {name: 'about', groups: ['about']},
-        
+
         {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
         {name: 'forms', groups: ['forms']},
         {name: 'insert', groups: ['insert']},
         {name: 'links', groups: ['links']},
-        
+
         {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
         {name: 'styles', groups: ['styles']},
         {name: 'colors', groups: ['colors']},
